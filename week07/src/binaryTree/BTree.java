ccd437268fcfcd4c217ed648d5e9f057fb00c62f
Thomas Howe <u1660884@unimail.hud.ac.uk>
2018-07-05 20:05:54 +0100

    Final Version

--- week07/src/binaryTree/BTree.java
+++ week07/src/binaryTree/BTree.java
@@ -1,12 +1,39 @@
 package binaryTree;
 
 public interface BTree<T extends Comparable<? super T>> {
+	/**
+	 * Find the first correct placement for a comparable value
+	 * in a binary tree and creates a new Node at that point
+	 * @param value An instance of a comparable data type 
+	 */
 	public void insert(T value);
-	
+	/**
+	 * Getter for the comparable data stored in a node
+	 * @return Instance of comparable data stored at the Node
+	 */
 	public T value();
-	
+	/**
+	 * Getter for the Node attached to the left branching path
+	 * this may return a null value.
+	 * @return Node attached to the left path of this Node
+	 */
 	public BTree<T> left();
-	
+	/**
+	 * Getter for the Node attached to the right branching path
+	 * this may return a null value.
+	 * @return Node attached to the right path of this Node
+	 */
 	public BTree<T> right();
+	/**
+	 * Calculates the size of the tree starting
+	 * at this instance of a Node.
+	 * @return Number of filled Nodes in the Binary Tree
+	 */
+	public int getSize();
+	/**
+	 * Getter for the comparable data stored in a node
+	 * @return Instance of comparable data stored at the Node
+	 */
+	public T getValue();
 
 }
--- week07/src/binaryTree/BinaryTreeTest.java
+++ week07/src/binaryTree/BinaryTreeTest.java
@@ -1,5 +1,71 @@
 package binaryTree;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class BinaryTreeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSize() {
+		BinaryTree<Integer> testTree = new BinaryTree<>();
+		testTree.insert(0);
+		testTree.insert(1);
+		testTree.insert(2);
+		testTree.insert(3);
+		testTree.insert(-1);
+		testTree.insert(-2);
+		testTree.insert(-3);
+		testTree.insert(-4);
+		assertEquals(8, testTree.getSize());
+	}
+	
+	@Test
+	public void testLeftNode() {
+		BinaryTree<Integer> testTree = new BinaryTree<>();
+		testTree.insert(0);
+		testTree.insert(2);
+		testTree.insert(4);
+		testTree.insert(6);
+		testTree.insert(-1);
+		testTree.insert(-2);
+		testTree.insert(-3);
+		testTree.insert(-4);
+		assertEquals(new Integer(-1), testTree.left().getValue());
+	}
+	
+	@Test
+	public void test2ndRightNode() {
+		BinaryTree<Integer> testTree = new BinaryTree<>();
+		testTree.insert(0);
+		testTree.insert(2);
+		testTree.insert(4);
+		testTree.insert(6);
+		testTree.insert(-1);
+		testTree.insert(-2);
+		testTree.insert(-3);
+		testTree.insert(-4);
+		assertEquals(new Integer(2), testTree.right().right().getValue());
+	}
 
 }
